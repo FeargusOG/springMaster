6374d9ff4f9b81142443c6c9e9dbbe717cb40178
Feargus O'Gorman <feargusogorman@gmail.com>
2015-09-16 17:22:14 +0100

    First Commit

---
+++ src/main/java/org/feargus/springmaster/Application.java
@@ -0,0 +1,28 @@
+package org.feargus.springmaster;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan
+//@SpringBootApplication //This annotation is the same as the other three put together
+public class Application implements CommandLineRunner {
+	
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    //@Override
+    public void run(String... strings) throws Exception {
+    	log.info("STARTING RUN....");    	
+    }
+}
