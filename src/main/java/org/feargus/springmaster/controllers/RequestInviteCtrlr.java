ea72fcf1a6f81e6cac66ee5fb52253ce9e6f09b4
Feargus O'Gorman <feargusogorman@gmail.com>
2015-10-04 11:48:35 +0100

    Added project param to request invite

--- src/main/java/org/feargus/springmaster/controllers/RequestInviteCtrlr.java
+++ src/main/java/org/feargus/springmaster/controllers/RequestInviteCtrlr.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RequestInviteCtrlr {
@@ -13,10 +14,10 @@ public class RequestInviteCtrlr {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	@RequestMapping("/requestInvite")
-	public String requestInvite(){
+	public String requestInvite(@RequestParam(value="projectName", required=true) String projectName){
 		String token = "blaher1";
     	Mailer mailSender = new Mailer();
-    	mailSender.sendMail("feargusogorman@gmail.com", "Invite Request", "I would like to join please! :) Token = "+token);
+    	mailSender.sendMail("feargusogorman@gmail.com", "Invite Request", "I would like to join please! :) Project = "+projectName+" Token = "+token);
     	
     	log.info("Finished sending a mail....");
 		
