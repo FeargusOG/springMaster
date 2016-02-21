ef2c68adf3f9f0bc141b2522d49c4faa41b4af6b
Feargus O'Gorman <feargusogorman@gmail.com>
2016-02-21 11:25:59 +0000

    Added basic acc creation form

--- src/main/java/org/feargus/springmaster/users/controllers/AccountCreationCtrlr.java
+++ src/main/java/org/feargus/springmaster/users/controllers/AccountCreationCtrlr.java
@@ -6,7 +6,6 @@ import org.feargus.springmaster.invites.controllers.RequestInviteCtrlr;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,23 +16,19 @@ public class AccountCreationCtrlr {
     private static final Logger log = LoggerFactory.getLogger(RequestInviteCtrlr.class);
 
     @RequestMapping(value = "/accountCreation", method = RequestMethod.GET)
-    public String getAccountCreation(Model model) {
-	model.addAttribute("accountCreation", new AccountCreationForm());
+    public String getAccountCreation(AccountCreationForm accForm) {
 	return "accountCreation";
     }
 
     @RequestMapping(value = "/accountCreation", method = RequestMethod.POST)
-    public String checkPersonInfo(@Valid AccountCreationForm accForm, BindingResult bindingResult, Model model) {
+    public String postAccountCreation(@Valid AccountCreationForm accForm, BindingResult bindingResult) {
 
 	if (bindingResult.hasErrors()) {
 	    return "accountCreation";
-	} else {
-	    model.addAttribute(accForm);
 	}
 
-	log.info("\n\nIn POST." + accForm.getName() + " - " + Integer.toString(accForm.getAge()) + "\n\n");
+	log.info("Creating an account for user: " + accForm.getName());
 
 	return "redirect:/home";
     }
-
 }
