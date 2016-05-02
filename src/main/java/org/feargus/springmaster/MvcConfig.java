a56e045219052c07edf50e203d6fe3ec86cf4bcf
Feargus O'Gorman <feargusogorman@gmail.com>
2016-05-02 10:56:38 +0100

    Added 403 cntrlr and page

--- src/main/java/org/feargus/springmaster/MvcConfig.java
+++ src/main/java/org/feargus/springmaster/MvcConfig.java
@@ -1,4 +1,4 @@
-package org.feargus.springmaster.controllers;
+package org.feargus.springmaster;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -10,6 +10,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 	registry.addViewController("/login").setViewName("login");
+	registry.addViewController("/403").setViewName("403");
     }
-
 }
