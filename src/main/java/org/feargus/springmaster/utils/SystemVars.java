f01a46d2e41bd9b9baaaa7159b721284bd425006
Feargus O'Gorman <feargusogorman@gmail.com>
2016-06-19 18:08:21 +0100

    cleaned up env vars

--- src/main/java/org/feargus/springmaster/utils/SystemVars.java
+++ src/main/java/org/feargus/springmaster/utils/SystemVars.java
@@ -1,13 +1,8 @@
 package org.feargus.springmaster.utils;
 
 public class SystemVars {
-    // static final public String dbUrl = "DATABASE_URL_TEST";
     static final public String DB_URL = "DATABASE_URL";
-
-    // static final public String rootUrl = "http://127.0.0.1:8080";
-    // static final public String rootUrl = "www.feargus.org";
     static final public String ROOT_URL = "ROOT_URL";
-
     static final public String ADMIN_EMAIL = "ADMIN_EMAIL";
     static final public String ADMIN_EMAIL_PSWRD = "ADMIN_EMAIL_PSWRD";
 }
