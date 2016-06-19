ec08b3a1f57676050637fefd637413536ee566b3
Feargus O'Gorman <feargusogorman@gmail.com>
2016-06-19 18:19:22 +0100

    updated gitignore

--- SpringMasterDocs/DDLs/InviteDDL.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE invitetokens(
-	token text PRIMARY KEY NOT NULL,
-	email text NOT NULL UNIQUE,
-	createtime timestamp WITH TIME ZONE DEFAULT current_timestamp
-);
-
-CREATE TABLE invitetokens(token char(40) PRIMARY KEY NOT NULL, email varchar(255) NOT NULL, createtime timestamp WITH TIME ZONE DEFAULT current_timestamp);
-ALTER TABLE invitetokens ADD UNIQUE (email);
--- SpringMasterDocs/DDLs/members_rolesDDL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE members_roles(
-	userEmail text NOT NULL references members(userNameEmail),
-	role text NOT NULL,
-	PRIMARY KEY(userEmail, role)
-);
-
-CREATE TABLE members_roles(userEmail text NOT NULL references members(userNameEmail), role text NOT NULL, PRIMARY KEY(userEmail, role));
-
-INSERT INTO members_roles(userEmail, role) VALUES (?,?)
