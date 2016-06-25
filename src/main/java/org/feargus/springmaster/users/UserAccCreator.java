a4ca88a287fee9e483a6c670a6286637cd55f0b5
Feargus O'Gorman <feargusogorman@gmail.com>
2016-06-25 22:40:41 +0100

    New email body

--- src/main/java/org/feargus/springmaster/users/UserAccCreator.java
+++ src/main/java/org/feargus/springmaster/users/UserAccCreator.java
@@ -41,10 +41,11 @@ public class UserAccCreator {
 	final String hashedUserEmail = userUtils.hashUserEmail(userObj.getSalt(), userObj.getUserNameEmail());
 	final String userConfirmationURL = SystemVars.getInstance().getROOT_URL()
 		+ "/accountActivation?userEmail=" + userObj.getUserNameEmail() + "&token=" + hashedUserEmail;
+	final String emailBody = "<html><body>Hi!<br /><br />Thanks for requesting an account for feargus.org!<p>Please follow this link to confirm your account: <a href=\""
+		+ userConfirmationURL + "\">Confirm Account</a></p>Thanks,<br />Feargus</body></html>";
+	final String emailSubject = "User Account Confirmation for feargus.org!";
 
 	mailSender.sendMail(userObj.getUserNameEmail(), SystemVars.getInstance().getADMIN_EMAIL(),
-		"User Account Confirmation for feargus.org!",
-		"Hi!\n\nThanks for requesting an account for feargus.org! Please follow this link to confirm your account: "
-			+ userConfirmationURL);
+		emailSubject, emailBody);
     }
 }
